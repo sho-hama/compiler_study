3bfefb30954dd018637e0f74b8d0fa9c202b3fc1
sho-hama <shokihamana@gmail.com>
2022-06-07 19:29:25 +0900

    add: test

---
+++ 9cc/test/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SRC_DIR=~/compiler_study/9cc/src
+
+assert() {
+  expected="$1"
+  input="$2"
+  $SRC_DIR/9cc "$input" > $SRC_DIR/tmp.s
+  cc -o $SRC_DIR/tmp $SRC_DIR/tmp.s
+  $SRC_DIR/tmp
+  actual="$?"
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+assert 0 0
+assert 42 42
+echo OK
